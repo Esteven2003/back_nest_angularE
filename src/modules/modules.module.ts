bc2b406f691b305718b9f99499dbbdfb30e86bb9
Esteven <bey.viracocha@yavirac.edu.ec>
2025-10-24 08:57:32 -0500

    migraciones

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './users/users.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, CategoriaModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
