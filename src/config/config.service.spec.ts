e735d9355b2c26e7f4837b83c0961be1ddac639c
Esteven2003 <bey.viracocha@yavirac.edu.ec>
2025-11-24 22:59:32 -0500

    Fix test setups with mocked dependencies

--- src/config/config.service.spec.ts
+++ src/config/config.service.spec.ts
@@ -1,10 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as fs from 'fs';
 import { ConfigService } from './config.service';
+
+jest.mock('fs', () => ({
+  existsSync: jest.fn(),
+  readFileSync: jest.fn(),
+}));
 
 describe('ConfigService', () => {
   let service: ConfigService;
 
   beforeEach(async () => {
+    (fs.existsSync as jest.Mock).mockReturnValue(true);
+    (fs.readFileSync as jest.Mock).mockReturnValue('KEY=value');
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [ConfigService],
     }).compile();
--- src/modules/producto/producto.service.spec.ts
+++ src/modules/producto/producto.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Producto } from './entities/producto.entity';
 import { ProductoService } from './producto.service';
+
+const mockProductoRepository = {};
 
 describe('ProductoService', () => {
   let service: ProductoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductoService],
+      providers: [
+        ProductoService,
+        {
+          provide: getRepositoryToken(Producto),
+          useValue: mockProductoRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductoService>(ProductoService);
